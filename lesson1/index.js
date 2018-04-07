3a71b1624f71da43efe67f0dd7ec1865bfd73193
Alexander Prokopenko <whitershade34@gmail.com>
2018-04-07 13:59:37 +0300

    added airbnb-base for eslint checking

--- lesson1/index.js
+++ lesson1/index.js
@@ -3,7 +3,7 @@ const yuriiObject = require('./module');
 const { world } = require('./getWorld');
 const badPractice = require('./badPracticeGlobalExport');
 
-console.log(`Hello, ${ world }`);
-console.log(`Current platform: ${ os.platform() }`);
+console.log(`Hello, ${world}`);
+console.log(`Current platform: ${os.platform()}`);
 console.log(yuriiObject);
-console.log(`console.log of global.badPractice is ${ global.badPractice }`);
+console.log(`console.log of global.badPractice is ${global.badPractice}`);
