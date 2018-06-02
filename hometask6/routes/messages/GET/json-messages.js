d9788a4c931cec34f5ef34995612986b97cab237
Alex Prokopenko <alex.r@didww.com>
2018-06-03 00:17:50 +0300

    refs #15, refactored sorting and filtering of message

--- hometask6/routes/messages/GET/json-messages.js
+++ hometask6/routes/messages/GET/json-messages.js
@@ -1,23 +1,34 @@
-const { orderBy, sortBy } = require('lodash');
 const fs = require('fs');
+const { orderBy } = require('lodash');
+
 
 module.exports = (req, res) => {
+  const filterOptions = ['user', 'show', 'message'];
+  const sortOptions = ['addedAt', 'endAt'];
+
   fs.readFile(req.app.locals.dataPath, 'utf8', (error, data) => {
     if (error) res.send(error);
 
     try {
       const { query } = req;
-
       let messages = JSON.parse(data);
 
-      const parameters = Object
-        .keys(query)
-        .filter(key => query[key] !== '');
+      const { filter: queryFilter, sort: querySort } = Object.keys(query).reduce((accumulator, key) => {
+        if (query[key] === '') return accumulator;
+        if (filterOptions.includes(key)) { return { ...accumulator, filter: [...accumulator.filter, key] }; }
+        if (sortOptions.includes(key)) { return { ...accumulator, sort: [...accumulator.sort, key] }; }
+
+        return accumulator;
+      }, { filter: [], sort: [] });
+
+      queryFilter.forEach((key) => {
+        messages = messages.filter(item => item[key] === query[key]);
+      });
 
       messages = orderBy(
         messages,
-        parameters,
-        parameters.map(key => query[key]),
+        querySort,
+        querySort.map(key => query[key]),
       );
 
       const json = JSON.stringify(messages);
--- hometask6/routes/messages/index.js
+++ hometask6/routes/messages/index.js
@@ -4,8 +4,7 @@ const getRoot = require('./GET');
 const postRoot = require('./POST');
 const jsonPage = require('./GET/json');
 const addNewMessage = require('./GET/new');
-const getJsonMessage = require('./GET/json-message');
-const getJsonMessages = require('./GET/json-messages');
+const getJsonMessage = require('./GET/json-messages');
 
 const router = express.Router();
 
@@ -14,5 +13,4 @@ module.exports = router
   .post('/', postRoot)
   .get('/new', addNewMessage)
   .get('/json', jsonPage)
-  .get('/get-json-message', getJsonMessage)
-  .get('/get-json-messages', getJsonMessages);
+  .get('/get-json-messages', getJsonMessage);
